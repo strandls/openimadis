e8fd6a8d92a6ea98a257d4fa3bdcf14817721446
Rahul Yadava <rahul@strandls.com>
2016-01-22 17:18:19 +0530

    Added script and task for creating server bundle

---
+++ buildServerBundle.sh
@@ -0,0 +1,14 @@
+gradle --rerun-tasks -p iserver build;
+gradle --rerun-tasks -p iserver generateAllWSDLs;
+gradle --rerun-tasks -p iserver generateAllSkeletons;
+gradle --rerun-tasks -p iserver updateSkeletons;
+gradle stopTomcat;
+gradle --rerun-tasks -p iserver deployContext;
+gradle --rerun-tasks -p iserver deployLibs;
+gradle startTomcat;
+gradle --rerun-tasks -p iserver deployAllServices;
+gradle --rerun-tasks -p iclient generateAllStubs;
+gradle stopTomcat;
+gradle --rerun-tasks -p web deployWebApp;
+gradle --rerun-tasks createServerBundle;
+
